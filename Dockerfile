60d1ddb9e7356aae39b1f199dae920b033e6e10f
Scott Hanselman <scott@hanselman.com>
2018-08-28 23:40:06 -0700

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,32 @@
+FROM microsoft/dotnet:2.1-sdk-alpine AS build
+WORKDIR /app
+
+# copy csproj and restore as distinct layers
+COPY *.sln .
+COPY nuget.config . 
+COPY superzeit/*.csproj ./superzeit/
+RUN dotnet restore
+
+# copy everything else and build app
+COPY . .
+WORKDIR /app/superzeit
+RUN dotnet build
+ 
+FROM build AS publish
+WORKDIR /app/superzeit
+# add IL Linker package (optional, can make things smaller. Can also be here, or in the actual csproj)
+#RUN dotnet add package ILLink.Tasks -v 0.1.5-preview-1841731 -s https://dotnet.myget.org/F/dotnet-core/api/v3/index.json
+# ALPINE SELF CONTAINED
+#RUN dotnet publish -c Release -o out -r linux-musl-x64 /p:ShowLinkerSizeComparison=true 
+# NOT SELF CONTAINED
+RUN dotnet publish -c Release -o out /p:ShowLinkerSizeComparison=true 
+
+# ALPINE SELF CONTAINED 
+#FROM microsoft/dotnet:2.1-runtime-deps-alpine AS runtime
+# NOT SELF CONTAINED
+FROM microsoft/dotnet:2.1-aspnetcore-runtime-alpine AS runtime
+
+ENV DOTNET_USE_POLLING_FILE_WATCHER=true
+WORKDIR /app
+COPY --from=publish /app/superzeit/out ./
+ENTRYPOINT ["dotnet", "superzeit.dll"]
---
+++ superzeit/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace superzeit
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                // .ConfigureAppConfiguration((builderContext, config) =>
+                // {
+                //     IHostingEnvironment env = builderContext.HostingEnvironment;
+                //     config.Sources.Clear(); //Don't use the default AddJsonFile
+                //     config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: false)
+                //         .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: false); 
+                //         //changed to reloadOnChange to false to better support Alpine and constrined environments that don't like a lot of File Watchers
+                //         // avoids errors like:
+                //         // The configured user limit (128) on the number of inotify instances has been reached
+                // })
+                .UseStartup<Startup>();
+    }
+}
